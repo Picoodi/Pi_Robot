d55cc36de924321f95189a9774ca8f8c4c58610c
MaxO <maxi.opel.16@gmail.com>
2025-09-13 23:23:53 +0200

    Adding the basic files

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import serial
+import keyboard
 import time
 
-serial = serial.Serial('COM8', 115200)  # Port anpassen (unter Linux z.B. /dev/ttyACM0)
+serial = serial.Serial('COM8', 115200)  # Port anpassen (unter Linux /dev/ttyACM0)
 time.sleep(2)  # Verbindung aufbauen lassen
+print("Start")
 
-serial.write(b'LED ON\n')
-time.sleep(1)
-serial.write(b'LED OFF\n')
+while True:
+    if keyboard.is_pressed("w"):
+        print("w is pressed")
+        serial.write(b'Forward\n')
+
+    elif keyboard.is_pressed("s"):
+        print("s is pressed")
+        serial.write(b"Backwards\n")
+
+    elif keyboard.is_pressed("a"):
+        print("a is pressed")
+        serial.write(b"Left\n")
+
+    elif keyboard.is_pressed("d"):
+        print("d is pressed")
+        serial.write(b"Right\n")
+
+    else:
+        serial.write(b"Stop\n")
+
 
 serial.close()
